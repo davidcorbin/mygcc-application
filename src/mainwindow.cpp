78574c13f5f1643157b100a703a8465e034103d9
David Corbin <daconex@gmail.com>
2017-12-20 16:33:38 -0500

    Added basic sidebar layout

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -9,6 +9,12 @@
 #include <QMainWindow>
 #include <QLocale>
 #include <QWidget>
+#include <QLabel>
+
+#include <QPushButton>
+#include <QPalette>
+#include <QStackedLayout>
+#include <QGridLayout>
 
 #include <iostream>
 
@@ -17,113 +23,37 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
         setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
+        setWindowTitle("Facade");
 
-       // setCentralWidget(this);
+        QWidget *sidebar = new QWidget();
+        sidebar->setStyleSheet("background-color: red");
+        // Sidebar width constraints
+        sidebar->setMaximumWidth(200);
+        sidebar->setMinimumWidth(200);
 
-       // createActions();
+        // Sidebar height constraints
+        sidebar->setMinimumHeight(400);
+
+        QWidget *mainBody = new QWidget();
+        mainBody->setStyleSheet("background-color: blue");
+
+        // Body width constraints
+        mainBody->setMinimumWidth(400);
+        mainBody->setMaximumWidth(800);
+
+        // Body height constraints
+        mainBody->setMinimumHeight(400);
+
+        QGridLayout *centralLayout = new QGridLayout;
+        centralLayout->addWidget(sidebar, 0, 0, -1, 1);
+        centralLayout->addWidget(mainBody, 0, 1, 1, 1);
+
+        QWidget *centralWidget = new QWidget;
+        centralWidget->setLayout(centralLayout);
+        centralWidget->setStyleSheet("background-color: rgb(50, 48, 52)");
+
+        setCentralWidget(centralWidget);
 }
-/*
-void MainWindow::createActions() {
-    QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
-    QToolBar *fileToolBar = addToolBar(tr("File"));
-    const QIcon newIcon = QIcon::fromTheme("document-new", QIcon(":/images/new.png"));
-    QAction *newAct = new QAction(newIcon, tr("&New"), this);
-    newAct->setShortcuts(QKeySequence::New);
-    newAct->setStatusTip(tr("Create a new file"));
-    connect(newAct, &QAction::triggered, this, &MainWindow::newFile);
-    fileMenu->addAction(newAct);
-    fileToolBar->addAction(newAct);
-
-//! [19]
-    const QIcon openIcon = QIcon::fromTheme("document-open", QIcon(":/images/open.png"));
-    QAction *openAct = new QAction(openIcon, tr("&Open..."), this);
-    openAct->setShortcuts(QKeySequence::Open);
-    openAct->setStatusTip(tr("Open an existing file"));
-    connect(openAct, &QAction::triggered, this, &MainWindow::open);
-    fileMenu->addAction(openAct);
-    fileToolBar->addAction(openAct);
-//! [18] //! [19]
-
-    const QIcon saveIcon = QIcon::fromTheme("document-save", QIcon(":/images/save.png"));
-    QAction *saveAct = new QAction(saveIcon, tr("&Save"), this);
-    saveAct->setShortcuts(QKeySequence::Save);
-    saveAct->setStatusTip(tr("Save the document to disk"));
-    connect(saveAct, &QAction::triggered, this, &MainWindow::save);
-    fileMenu->addAction(saveAct);
-    fileToolBar->addAction(saveAct);
-
-    const QIcon saveAsIcon = QIcon::fromTheme("document-save-as");
-    QAction *saveAsAct = fileMenu->addAction(saveAsIcon, tr("Save &As..."), this, &MainWindow::saveAs);
-    saveAsAct->setShortcuts(QKeySequence::SaveAs);
-    saveAsAct->setStatusTip(tr("Save the document under a new name"));
-
-//! [20]
-
-    fileMenu->addSeparator();
-
-    const QIcon exitIcon = QIcon::fromTheme("application-exit");
-    QAction *exitAct = fileMenu->addAction(exitIcon, tr("E&xit"), this, &QWidget::close);
-    exitAct->setShortcuts(QKeySequence::Quit);
-//! [20]
-    exitAct->setStatusTip(tr("Exit the application"));
-
-//! [21]
-    QMenu *editMenu = menuBar()->addMenu(tr("&Edit"));
-    QToolBar *editToolBar = addToolBar(tr("Edit"));
-//!
-#ifndef QT_NO_CLIPBOARD
-    const QIcon cutIcon = QIcon::fromTheme("edit-cut", QIcon(":/images/cut.png"));
-    QAction *cutAct = new QAction(cutIcon, tr("Cu&t"), this);
-//! [21]
-    cutAct->setShortcuts(QKeySequence::Cut);
-    cutAct->setStatusTip(tr("Cut the current selection's contents to the "
-                            "clipboard"));
-    connect(cutAct, &QAction::triggered, textEdit, &QPlainTextEdit::cut);
-    editMenu->addAction(cutAct);
-    editToolBar->addAction(cutAct);
-
-    const QIcon copyIcon = QIcon::fromTheme("edit-copy", QIcon(":/images/copy.png"));
-    QAction *copyAct = new QAction(copyIcon, tr("&Copy"), this);
-    copyAct->setShortcuts(QKeySequence::Copy);
-    copyAct->setStatusTip(tr("Copy the current selection's contents to the "
-                             "clipboard"));
-    connect(copyAct, &QAction::triggered, textEdit, &QPlainTextEdit::copy);
-    editMenu->addAction(copyAct);
-    editToolBar->addAction(copyAct);
-
-    const QIcon pasteIcon = QIcon::fromTheme("edit-paste", QIcon(":/images/paste.png"));
-    QAction *pasteAct = new QAction(pasteIcon, tr("&Paste"), this);
-    pasteAct->setShortcuts(QKeySequence::Paste);
-    pasteAct->setStatusTip(tr("Paste the clipboard's contents into the current "
-                              "selection"));
-    connect(pasteAct, &QAction::triggered, textEdit, &QPlainTextEdit::paste);
-    editMenu->addAction(pasteAct);
-    editToolBar->addAction(pasteAct);
-
-    menuBar()->addSeparator();
-
-#endif // !QT_NO_CLIPBOARD
-
-    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
-    QAction *aboutAct = helpMenu->addAction(tr("&About"), this, &MainWindow::about);
-    aboutAct->setStatusTip(tr("Show the application's About box"));
-
-//! [22]
-
-    QAction *aboutQtAct = helpMenu->addAction(tr("About &Qt"), qApp, &QApplication::aboutQt);
-    aboutQtAct->setStatusTip(tr("Show the Qt library's About box"));
-//! [22]
-
-//! [23]
-#ifndef QT_NO_CLIPBOARD
-    cutAct->setEnabled(false);
-//! [23] //! [24]
-    copyAct->setEnabled(false);
-    connect(textEdit, &QPlainTextEdit::copyAvailable, cutAct, &QAction::setEnabled);
-    connect(textEdit, &QPlainTextEdit::copyAvailable, copyAct, &QAction::setEnabled);
-#endif // !QT_NO_CLIPBOARD
-}
-*/
 
 MainWindow::~MainWindow() {
 }
