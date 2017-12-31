bbfd40b2e624933bb7a2d3fe660623fb6f484cc6
David Corbin <daconex@gmail.com>
2017-12-31 00:25:09 -0500

    Removed unecesscary commands from build script

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -11,7 +11,6 @@ sudo apt-get update
 sudo apt-get install -y git build-essential cmake qt5-default libssl-dev gcc-4.9 g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
-git clone git@github.com:davidcorbin/mygcc-application.git
 git submodule update --init --recursive
 mkdir build
 cd build
