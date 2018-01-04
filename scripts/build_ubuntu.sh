a8937e8b2570922d42dd08853fe328e0e72279cf
David Corbin <daconex@gmail.com>
2018-01-03 23:09:18 -0500

    Added qt version check
    Hopefully the macro will disable high dpi scaling on versions lower than
    5.6

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -8,20 +8,12 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install -y git build-essential cmake libssl-dev gcc-4.9 g++-4.9
+sudo apt-get install -y git build-essential cmake qt5-default libssl-dev gcc-4.9 g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
-# Install Qt from source
-if [ ! -e Qt/5.10 ] ; then
-  rm -rf Qt ;
-  wget http://download.qt.io/official_releases/qt/5.10/5.10.0/qt-opensource-linux-x64-5.10.0.run ;
-  chmod +x ./qt-opensource-linux-x64-5.10.0.run ;
-  ./qt-opensource-linux-x64-5.10.0.run --platform minimal --script scripts/qt-installer-noninteractive.qs --no-force-installations ;
-fi
-
+git clone git@github.com:davidcorbin/mygcc-application.git
 git submodule update --init --recursive
 mkdir build
 cd build
 cmake ..
 make
-
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,20 @@
  * Copyright 2018 <David Corbin, Mitchell Harvey>
  */
 
-#include <QApplication>
-
 #include <include/ui/MainWindow.hpp>
-
+#include <QApplication>
+#include <QtGlobal>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("facade");
     QCoreApplication::setApplicationName("Facade");
     QCoreApplication::setApplicationVersion(QT_VERSION_STR);
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);  // DPI support
+
+    //  High DPI support
+    #if QT_VERSION > QT_VERSION_CHECK(5, 6, 0)
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    #endif
 
     MainWindow w;
     w.show();
