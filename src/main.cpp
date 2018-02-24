3121528ea24cc76bfcba4dbe38b347574390a5cd
davidcorbin <daconex@gmail.com>
2018-02-24 16:46:51 -0500

    Set scale factor before application is instantiated

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 #include <QtGlobal>
 
 int main(int argc, char *argv[]) {
+	//  Scale GUI properly on Windows
+	#if QT_VERSION > QT_VERSION_CHECK(5, 6, 0)
+	qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+	#endif
+
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("facade");
     QCoreApplication::setApplicationName("Facade");
