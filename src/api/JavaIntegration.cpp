46477946480950f487e7dee13e17a296f83f0887
David Corbin <daconex@gmail.com>
2018-04-06 20:21:17 -0400

    Added support for java 32 and 64 bit versions

--- src/api/JavaIntegration.cpp
+++ src/api/JavaIntegration.cpp
@@ -9,6 +9,8 @@
 #include <QFileInfo>
 #include <QTextStream>
 #include <QDebug>
+#include <QSettings>
+#include <QApplication>
 #include <string>
 #include <algorithm>
 #include <random>
@@ -45,7 +47,7 @@ void JavaIntegration::startAPIServerCmd() {
                             + jarPath + "\" com.mygcc.api.Main";
       javaProcess.start(fullStr.c_str());
     } else {
-      qDebug("%s", "Invalid java version");
+      qDebug() << "Invalid java version";
 
       // Open site to show user how to install java
       QDesktopServices::openUrl(QUrl(INSTALL_JAVA_SITE));
@@ -77,9 +79,61 @@ std::string* JavaIntegration::findJava() {
     return new std::string(JAVA_PATH_MAC);
   } else {
     qDebug() << "Known macOS java path NOT found";
+    return new std::string("");
   }
   #endif
 
+  // User registry keys for finding java on Windows
+  #if defined(_WIN32) || defined(_WIN64)
+  // 64 bit registry key
+  QSettings jreKey64("HKEY_LOCAL_MACHINE\\SOFTWARE" \
+      "\\JavaSoft\\Java Runtime Environment",
+      QSettings::Registry64Format);
+
+  // 32 bit registry key
+  QSettings jreKey32("HKEY_LOCAL_MACHINE\\Software\\WOW6432Node" \
+      "\\JavaSoft\\Java Runtime Environment",
+      QSettings::Registry32Format);
+
+  // If JAVA installed
+  if (jreKey32.allKeys().contains("CurrentVersion", Qt::CaseInsensitive) ||
+      jreKey64.allKeys().contains("CurrentVersion", Qt::CaseInsensitive)) {
+    QSettings *jreKey;
+
+    if (jreKey32.allKeys().contains("CurrentVersion", Qt::CaseInsensitive)) {
+      qDebug("32 bit Java FOUND");
+      jreKey = &jreKey32;
+    }
+    if (jreKey64.allKeys().contains("CurrentVersion", Qt::CaseInsensitive)) {
+      qDebug("64 bit JAVA FOUND");
+      jreKey = &jreKey64;
+    }
+
+    auto version = jreKey->value("CurrentVersion").toString();
+    qDebug() << "Latest java version identified:" <<
+        version.toStdString().c_str();
+    if (version.contains("1.8")) {
+      QSettings jreVerKey(jreKey->fileName() + "\\" + version,
+          QSettings::Registry64Format);
+
+      if (jreVerKey.childKeys().contains("JavaHome", Qt::CaseInsensitive)) {
+        auto javaHome = jreVerKey.value("JavaHome").toString();
+        qDebug() << "Java home identified:" << javaHome.toStdString().c_str();
+
+        return new std::string(javaHome.toStdString() + "\\bin\\java.exe");
+      } else {
+        qWarning("JavaHome key NOT FOUND");
+      }
+    } else {
+      qWarning("Java version '1.8' NOT FOUND");
+    }
+  } else {
+    qWarning("Java NOT FOUND in default install path");
+  }
+  return new std::string("");
+  #endif
+
+  #if defined(__unix) || defined(__unix__) || defined(__linux__)
   // Check for java in $PATH
   QProcess findJavaExe;
   findJavaExe.start("java");
@@ -96,6 +150,7 @@ std::string* JavaIntegration::findJava() {
   } else {
     return new std::string("");
   }
+  #endif
 }
 
 bool JavaIntegration::checkJavaVersion(std::string *javaPath) {
