b8218e0f00fcc324e6f9bb8dfca3b5dc794dae25
David Corbin <daconex@gmail.com>
2018-10-01 14:38:47 -0400

    Stop cloning submodules on travis

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -11,7 +11,6 @@ sudo apt-get update
 sudo apt-get install -y git build-essential cmake qt5-default libqt5svg5* libssl-dev gcc-4.9 g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
-git submodule update --init --recursive
 mkdir build
 cd build
 cmake ..
